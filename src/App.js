7458ddf36a77a7dd5023b61da0006e36ec220234
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-04-24 19:38:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import NewUser from './components/NewUser'
+import UsersList from './components/UsersList'
+import { UserProvider } from './context/UserContext'
+
+
+
+function App() {
+  return (
+    <UserProvider>
+      <h1>App is working</h1>
+      <UsersList />
+      <NewUser />
+    </UserProvider>
+  )
+}
+
+export default App
